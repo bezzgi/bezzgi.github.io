c285f4c014c1e9e4ac643428ed7c5639776fad3a
bezzgi <salathe.beat@bluewin.ch>
2021-09-01 15:12:01 +0200

    initial commit

---
+++ php/formular.php
@@ -0,0 +1,10 @@
+<?php
+    	echo '<u> Deine Angaben: </u> <br><br>';
+    	echo 'Dein Vorname: ' .$_POST['vorname'] .'<br>';
+    	echo 'Dein Nachname: ' .$_POST['nachname'] .'<br>';
+    	echo 'Deine E-Mail: ' .$_POST['e-mail'] .'<br>';
+    	echo 'Deine Handynummer: ' .$_POST['tel'] .'<br>';
+    	echo 'Deine Probleme: ' .$_POST['probleme'] .'<br>';
+    	echo 'Dein Feedback: ' .$_POST['feedback'] .'<br>';
+	echo 'Sontiges: ' .$_POST['sonstiges'] .'<br>';
+?>
